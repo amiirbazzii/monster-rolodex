0d92886fd2a6e4ea92cc6815062bcb339a48e1a3
amiirbazzii <amir.hossein.b.a1011@gmail.com>
2019-12-10 22:29:57 -0800

    Updates

---
+++ precache-manifest.ce5f336500d83fefaa2c6d782bff7f9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adb8debcffbd9b0ebc1a7e4398745661",
+    "url": "/amiirbazzii/monster-rolodex/index.html"
+  },
+  {
+    "revision": "bb55e0bdc7385be4aa08",
+    "url": "/amiirbazzii/monster-rolodex/static/css/main.5a5e5285.chunk.css"
+  },
+  {
+    "revision": "c728ba4a2366e885ec54",
+    "url": "/amiirbazzii/monster-rolodex/static/js/2.d8d20882.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/amiirbazzii/monster-rolodex/static/js/2.d8d20882.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bb55e0bdc7385be4aa08",
+    "url": "/amiirbazzii/monster-rolodex/static/js/main.90308070.chunk.js"
+  },
+  {
+    "revision": "923fdee28ca1bab5f48c",
+    "url": "/amiirbazzii/monster-rolodex/static/js/runtime-main.d79d19f3.js"
+  }
+]);
